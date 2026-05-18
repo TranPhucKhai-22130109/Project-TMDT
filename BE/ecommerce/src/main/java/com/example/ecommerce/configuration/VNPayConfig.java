68238bf91daa696a232587ef6df15a1bad315e00
LyThaiMinhKhang-22130113 <22130113@st.hcmuaf.edu.vn>
2026-05-18 23:05:18 +0700

    fix conflit

--- BE/ecommerce/src/main/java/com/example/ecommerce/configuration/VNPayConfig.java
+++ BE/ecommerce/src/main/java/com/example/ecommerce/configuration/VNPayConfig.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.ecommerce.configuration;
 
 import lombok.Getter;
@@ -26,33 +26,6 @@ public class VNPayConfig {
     public String getPaymentUrl() {
         return url;
     }
-=======
-package com.example.ecommerce.configuration;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConfigurationProperties(prefix = "vnpay")
-@Getter
-@Setter
-public class VNPayConfig {
-
-    private String tmnCode;
-    private String hashSecret;
-    private String url;
-    private String returnUrl;
-    private String version;
-    private String command;
-    private String orderType;
-    private String locale;
-    private String currencyCode;
-
-    // Alias cho VNPayService dùng getPaymentUrl()
-    public String getPaymentUrl() {
-        return url;
-    }
->>>>>>> b5ba13d (updated payment)
 }
